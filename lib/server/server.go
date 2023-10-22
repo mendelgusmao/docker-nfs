c6201cc2dd4ad06fd173221e41a1d80512671a11
MendelGusmao <mendelsongusmao@gmail.com>
2023-10-22 20:13:02 -0300

    add server

---
+++ lib/server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/willscott/memphis"
+
+	nfs "github.com/willscott/go-nfs"
+	nfshelper "github.com/willscott/go-nfs/helpers"
+)
+
+type Server struct {
+	nfs.Server
+	listener net.Listener
+}
+
+func Create(path string) (*Server, error) {
+	listener, err := net.Listen("tcp", ":0")
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen: %v\n", err)
+	}
+
+	fs := memphis.FromOS(path)
+	bfs := fs.AsBillyFS(0, 0)
+	handler := nfshelper.NewNullAuthHandler(bfs)
+	cacheHelper := nfshelper.NewCachingHandler(handler, 1024)
+
+	s := &Server{
+		listener: listener,
+	}
+	s.Handler = cacheHelper
+	s.Context = context.Background()
+
+	log.Printf("serving %s at %s\n", path, listener.Addr())
+
+	return s, nil
+}
+
+func (s *Server) Serve() error {
+	return s.Server.Serve(s.listener)
+}
+
+func (s *Server) Stop() {
+	s.Context.Done()
+}
